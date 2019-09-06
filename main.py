6509eb7c83b1d82323b84ebe1123535beb69c026
Anand <tilak@totaldigilution.com>
2019-09-06 15:29:13 +0530

    contact_Us_form_add

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask , render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return render_template('blog.html')
+    return render_template('blog.html')
+
 
 @app.route('/home')
 def home():
-  return render_template('page.html')
+    return render_template('page.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/contact_form',methods=['GET','POST'])
+def contact_form():
+    print(request.form['name'])
 
 app.run()
